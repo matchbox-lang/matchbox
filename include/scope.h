c5f6329112e3387cbc3b0fd5630d28a41e9b00d9
thedrumchannell <thedrumchannell@gmail.com>
2024-10-12 14:40:36 -0700

    Remove unused declaration

--- include/scope.h
+++ include/scope.h
@@ -20,7 +20,6 @@ Scope* createScope(Scope* parent);
 void freeScope(Scope* scope);
 size_t getLocalCount(Scope* scope);
 AST* setLocalSymbol(Scope* scope, StringObject* id, AST* symbol, bool local);
-AST* setLocalVariable(Scope* scope, StringObject* id, AST* symbol);
 AST* getLocalSymbol(Scope* scope, StringObject* id);
 AST* getSymbol(Scope* scope, StringObject* id);
 

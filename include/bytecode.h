ba29092a3fde4b7064d692c08e5fa7f8cae8bc8f
thedrumchannell <thedrumchannell@gmail.com>
2024-12-30 12:49:04 -0700

    Replace underscores for readability

--- include/bytecode.h
+++ include/bytecode.h
@@ -11,22 +11,22 @@ typedef enum Opcode
     OP_LDG,         // ldg imm8
     OP_STG,         // stg imm8
     OP_LDL,         // ldl imm8
-    OP_LDL_0,       // ldl 0
-    OP_LDL_1,       // ldl 1
-    OP_LDL_2,       // ldl 2
-    OP_LDL_3,       // ldl 3
+    OP_LDL_0,       // ldl_0
+    OP_LDL_1,       // ldl_1
+    OP_LDL_2,       // ldl_2
+    OP_LDL_3,       // ldl_3
     OP_STL,         // stl imm8
-    OP_STL_0,       // stl 0
-    OP_STL_1,       // stl 1
-    OP_STL_2,       // stl 2
-    OP_STL_3,       // stl 3
+    OP_STL_0,       // stl_0
+    OP_STL_1,       // stl_1
+    OP_STL_2,       // stl_2
+    OP_STL_3,       // stl_3
     OP_PUSHB,       // push imm8
     OP_PUSHH,       // push imm16
-    OP_PUSH_N1,     // push -1
-    OP_PUSH_0,      // push 0
-    OP_PUSH_1,      // push 1
-    OP_PUSH_2,      // push 2
-    OP_PUSH_3,      // push 3
+    OP_PUSH_N1,     // push_n1
+    OP_PUSH_0,      // push_0
+    OP_PUSH_1,      // push_1
+    OP_PUSH_2,      // push_2
+    OP_PUSH_3,      // push_3
     OP_POP,         // pop
     OP_DUP,         // dup
     OP_INC,         // inc

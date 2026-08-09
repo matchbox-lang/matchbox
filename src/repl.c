d1a8944dc09b57c5f5741966e507fb86350c72c6
thedrumchannell <thedrumchannell@gmail.com>
2026-08-09 13:17:39 -0700

    Preserve source history during interactive mode

--- src/repl.c
+++ src/repl.c
@@ -2,6 +2,8 @@
 #include "buffer.h"
 #include "compiler.h"
 #include "module_object.h"
+#include "util.h"
+#include "vector.h"
 #include "vm.h"
 #include <stdbool.h>
 #include <stddef.h>
@@ -50,6 +52,29 @@ static bool appendLine(char** source, size_t* size, size_t* length)
     return true;
 }
 
+static void unexpectedEndError(Compiler* compiler)
+{
+    Token token = compiler->parser.currentToken;
+
+    fprintf(stderr, "Error: Unexpected end of input on line %d:%d\n", token.line, token.column);
+    exit(1);
+}
+
+static bool finishSourceRead(Compiler* compiler, bool read, size_t length)
+{
+    if (read) {
+        return true;
+    }
+
+    printf("\n");
+
+    if (length > 0) {
+        unexpectedEndError(compiler);
+    }
+
+    return false;
+}
+
 static bool readSource(Compiler* compiler, char** source, size_t* size)
 {
     size_t length = 0;
@@ -64,30 +89,78 @@ static bool readSource(Compiler* compiler, char** source, size_t* size)
         complete = read && compileRepl(compiler, *source);
     }
 
-    if (!read) {
-        printf("\n");
+    return finishSourceRead(compiler, read, length);
+}
+
+static void freeReplSources(Vector* sources)
+{
+    size_t count = countVector(sources);
+
+    for (size_t i = 0; i < count; i++) {
+        free(getVectorAt(sources, i));
     }
 
-    return read;
+    freeVector(sources);
+}
+
+static void appendReplHistory(
+    char** history, size_t* size, size_t* length, const char* source)
+{
+    size_t sourceLength = strlen(source);
+    size_t required = *length + sourceLength + 1;
+
+    if (required > *size && !resizeSource(history, size, required)) {
+        outOfMemoryError();
+    }
+
+    memcpy(*history + *length, source, sourceLength + 1);
+    *length += sourceLength;
+}
+
+static void validateReplHistory(char* history)
+{
+    ModuleObject* module = createModuleObject();
+    Compiler compiler;
+
+    initCompiler(&compiler, module);
+
+    if (!compile(&compiler, history)) {
+        unexpectedEndError(&compiler);
+    }
+
+    freeCompiler(&compiler);
+    freeModuleObject(module);
 }
 
 void runRepl()
 {
     char* source = NULL;
     size_t size = 0;
+    char* history = NULL;
+    size_t historySize = 0;
+    size_t historyLength = 0;
     ModuleObject* module = createModuleObject();
     Compiler compiler;
     VM vm;
+    Vector sources;
     
     initCompiler(&compiler, module);
     initVM(&vm, module);
+    initVector(&sources);
 
     while (readSource(&compiler, &source, &size)) {
+        appendReplHistory(&history, &historySize, &historyLength, source);
+        validateReplHistory(history);
+        pushVectorItem(&sources, source);
+        source = NULL;
+        size = 0;
         interpret(&vm);
     }
 
     freeVM(&vm);
     freeCompiler(&compiler);
     freeModuleObject(module);
+    freeReplSources(&sources);
     free(source);
+    free(history);
 }

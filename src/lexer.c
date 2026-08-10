baa3e1898861e96b19188a5f62b44b249a0ab363
thedrumchannell <thedrumchannell@gmail.com>
2026-08-10 12:55:36 -0700

    Add dedicated function to scan float literals

--- src/lexer.c
+++ src/lexer.c
@@ -298,7 +298,10 @@ static void scanDigits(Lexer* lexer, bool (*isValidDigit)(char))
 
 static void validateNumberEnd(Lexer* lexer)
 {
-    if (isAlpha(peek(lexer)) || isDigit(peek(lexer)) || peek(lexer) == '_') {
+    bool adjacentDecimal = peek(lexer) == '.' && isDigit(next(lexer));
+
+    if (isAlpha(peek(lexer)) || isDigit(peek(lexer)) || peek(lexer) == '_'
+        || adjacentDecimal) {
         invalidNumberError(lexer);
     }
 }
@@ -410,13 +413,8 @@ static void scanExponent(Lexer* lexer)
     scanDigits(lexer, isDigit);
 }
 
-static Token scanFloatLiteral(Lexer* lexer)
+static Token finishFloatLiteral(Lexer* lexer)
 {
-    if (peek(lexer) == '.' && next(lexer) != '.') {
-        advance(lexer);
-        scanDigits(lexer, isDigit);
-    }
-
     if (peek(lexer) == 'e' || peek(lexer) == 'E') {
         scanExponent(lexer);
     }
@@ -424,6 +422,16 @@ static Token scanFloatLiteral(Lexer* lexer)
     validateNumberEnd(lexer);
 
     return makeToken(lexer, TOKEN_FLOAT_LITERAL);
+}
+
+static Token scanFloatLiteral(Lexer* lexer)
+{
+    if (peek(lexer) == '.' && next(lexer) != '.') {
+        advance(lexer);
+        scanDigits(lexer, isDigit);
+    }
+
+    return finishFloatLiteral(lexer);
 }
 
 static Token scanIntegerLiteral(Lexer* lexer)
@@ -693,7 +701,7 @@ Token scanToken(Lexer* lexer)
             if (isDigit(peek(lexer))) {
                 scanDigits(lexer, isDigit);
 
-                return scanFloatLiteral(lexer);
+                return finishFloatLiteral(lexer);
             }
             return makeToken(lexer, 
                 match(lexer, '.') ?
